c8b6ccf59fe31369d73bd382d98afc350841ea4b
Anderson Lopes <123224432+LopesA95@users.noreply.github.com>
2024-05-14 15:06:38 +0000

    ✨ feat: Update greeting routes to include exclamation marks

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res)
-    {res.send('Hello World')
+    {res.send('Hello World!')
 })
 
 app.get('/oi', function (req, res)
-    {res.send('Olá mundo!')
+    {res.send('Olá mundo!!!')
 })
 
 
