bbbdd0fdfb782cd8d6f14aad2fc14b6982c7cf7f
Anderson Lopes <123224432+LopesA95@users.noreply.github.com>
2024-05-14 18:02:03 +0000

    ✨ chore: Update cSpell dictionary and fix indentation in index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+  res.send('Hello World')
 })
 
 // Desafio: criar uma rota [GET] /oi que retorna "Olá, mundo!"
 app.get('/oi', function (req, res) {
-    res.send('Olá, mundo!')
+  res.send('Olá, mundo!')
 })
 
 // Lista de Itens
@@ -16,19 +16,38 @@ const itens = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
 // Endpoint de Read All [GET] /item
 app.get('/item', function (req, res) {
-    res.send(itens)
+  res.send(itens)
 })
 
 // Endpoint de Read By ID [GET] /item/:id
 app.get('/item/:id', function (req, res) {
   // Acessamos o parâmetro de rota ID
-    const id = req.params.id
+  const id = req.params.id
 
   // Acessamos o item na lista usando o ID - 1
-    const item = itens[id - 1]
+  const item = itens[id - 1]
 
   // Enviamos o item encontrado como resposta
-    res.send(item)
+  res.send(item)
+})
+
+// Sinalizamos que todo corpo de requisição
+// virá como JSON
+app.use(express.json())
+
+// Endpoint de Create [POST] /item
+app.post('/item', function (req, res) {
+  // Acessamos o corpo da requisição
+  const body = req.body
+
+  // Acessar o item no corpo da requisição
+  const novoItem = body.nome
+
+  // Adicionar o novo item na lista
+  itens.push(novoItem)
+
+  // Enviar uma mensagem de sucesso
+  res.send('Item adicionado com sucesso: ' + novoItem)
 })
 
 app.listen(3000)
