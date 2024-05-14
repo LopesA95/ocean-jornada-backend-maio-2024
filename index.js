52eb83bd9fd1876f168107712f3073183479aa20
Anderson Lopes <123224432+LopesA95@users.noreply.github.com>
2024-05-14 17:36:06 +0000

    ✨ feat: Add route for retrieving items by ID

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res)
-    {res.send('Hello World!')
+app.get('/', function (req, res) {
+    res.send('Hello World')
 })
 
-app.get('/oi', function (req, res)
-    {res.send('Olá mundo!!!')
+// Desafio: criar uma rota [GET] /oi que retorna "Olá, mundo!"
+app.get('/oi', function (req, res) {
+    res.send('Olá, mundo!')
 })
 
+// Lista de Itens
+const itens = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+//              0               1              2
 
+// Endpoint de Read All [GET] /item
+app.get('/item', function (req, res) {
+    res.send(itens)
+})
+
+// Endpoint de Read By ID [GET] /item/:id
+app.get('/item/:id', function (req, res) {
+  // Acessamos o parâmetro de rota ID
+    const id = req.params.id
+
+  // Acessamos o item na lista usando o ID - 1
+    const item = itens[id - 1]
+
+  // Enviamos o item encontrado como resposta
+    res.send(item)
+})
 
 app.listen(3000)
